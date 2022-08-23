8bf21e400b4d152af41342a698f423334f4a944e
Moses Kivua <kivuamoses9@gmail.com>
2022-08-23 15:23:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+// code your solution
+function saturdayFun (activity = "roller-skate"){
+    return ( `This Saturday, I want to ${activity}!`);
+}
+ const mondayWork = function (activity = "go to the office") {
+    return (`This Monday, I will ${activity}.` );
+ }
+function wrapAdjective (sasa = '*'){
+    const inner = function (adjective= "special") {
+        return `You are ${sasa}${adjective}${sasa}!`
+    }
+    return inner 
+}
